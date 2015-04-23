0a8c9ab2326020d5234cf62b76a3a862c2ff99a7
LittleFatZhang <https://LittleFatZhang@github.com>
2015-04-23 21:08:39 +0800

    add the exception catcher of first function

--- COM.KWS/COM.KWS/src/actionconverge/PageOpen.java
+++ COM.KWS/COM.KWS/src/actionconverge/PageOpen.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 public class PageOpen {
 	public int open(String url,String webbrower)
 	{
-		switch(webbrower)
+		try
+		{
+			switch(webbrower)
 			{
 				case "chrome":
 					WebDriver driver = new ChromeDriver();
@@ -18,13 +20,24 @@ public class PageOpen {
 					WebDriver driver2 = new InternetExplorerDriver();
 					driver2.get(url);
 				default:
-					System.out.println("Please choose the right Web Brower");
+					System.out.println("请在用例中写入正确的浏览器名称，例如:chrome,firefox,IE。");
 			}
+		}
+		catch(IllegalStateException e)
+		{
+			//e.printStackTrace();
+			System.out.println("请确认拥有对应浏览器的Selenium驱动。");
+		}
+		catch(WebDriverException e2)
+		{
+			//e2.printStackTrace();
+			System.out.println("请确认安装了对应的浏览器。");
+		}
 		return 1;
 	}
 	public static void main(String[] args)
 	{
 		PageOpen hehe = new PageOpen();
-		hehe.open("http://www.baidu.com","IE");
+		hehe.open("http://www.baidu.com","firefox");
 	}
 }
